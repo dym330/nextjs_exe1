242bdc29f67ab164a4dff23d016e2f44452d730b
YukiYamada <cat.of.attic@gmail.com>
2021-05-31 11:57:52 +0900

    [Add] Layout component

---
+++ pages/contact-page.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Contact = () => {
+  return (
+    <Layout title="Contact">
+      contact page
+    </Layout>
+  )
+}
+
+export default Contact;
